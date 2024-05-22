85bcc509dddf6655250a596bbc6d3bb65b253424
Thanaphon Phumthan <68098235+MoDangKa@users.noreply.github.com>
2024-05-22 18:57:46 +0700

    perf: update code

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,13 +2,14 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_riverpod_tutorial/widgets/custom_divider.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:logger/logger.dart';
 
 import '../main.dart';
+import '../widgets/custom_divider.dart';
 import '../widgets/custom_text_field.dart';
-import '../widgets/my_text_field_widget.dart';
 import '../widgets/my_text_field.dart';
+import '../widgets/my_text_field_widget.dart';
 
 class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
@@ -18,12 +19,16 @@ class HomeScreen extends ConsumerStatefulWidget {
 }
 
 class _HomeScreenState extends ConsumerState<HomeScreen> {
+  final Logger logger = Logger(printer: PrettyPrinter());
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _ageController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
+    final user = ref.read(userProvider);
+    _nameController.text = user.name;
+    _ageController.text = user.age.toString();
     _nameController.addListener(_onNameChanged);
     _ageController.addListener(_onAgeChanged);
   }
@@ -40,14 +45,9 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   }
 
   void onChangeAge(WidgetRef ref, String value) {
-    try {
-      double doubleValue = double.tryParse(value) ?? 0;
-      int parsedValue = doubleValue.toInt();
-      int currentValue = max(0, parsedValue);
-      ref.read(userProvider.notifier).updateAge(currentValue);
-    } catch (e) {
-      ref.read(userProvider.notifier).updateAge(0);
-    }
+    final currentValue =
+        value.isNotEmpty ? max(0, int.tryParse(value) ?? 0) : 0;
+    ref.read(userProvider.notifier).updateAge(currentValue);
   }
 
   void _onNameChanged() {
@@ -57,13 +57,25 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
 
   void _onAgeChanged() {
     try {
-      String newAge = _ageController.text;
-      double doubleValue = double.tryParse(newAge) ?? 0;
-      int parsedValue = doubleValue.toInt();
-      int currentValue = max(0, parsedValue);
+      final newAge = _ageController.text;
+      int currentValue =
+          newAge.isNotEmpty ? max(0, int.tryParse(newAge) ?? 0) : 0;
       ref.read(userProvider.notifier).updateAge(currentValue);
     } catch (e) {
       ref.read(userProvider.notifier).updateAge(0);
+    }
+  }
+
+  @override
+  void didUpdateWidget(covariant HomeScreen oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    final user = ref.read(userProvider);
+    if (_nameController.text != user.name) {
+      _nameController.text = user.name;
+    }
+    final ageText = user.age.toString();
+    if (_ageController.text != ageText) {
+      _ageController.text = ageText;
     }
   }
 
@@ -88,36 +100,42 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
             const SizedBox(height: 15),
             const Text("CustomTextField"),
             const SizedBox(height: 15),
-            CustomTextField(onChanged: (value) => onChangeName(ref, value)),
+            CustomTextField(
+              onChanged: (value) => setState(() => onChangeName(ref, value)),
+            ),
             const SizedBox(height: 15),
             CustomTextField(
-                type: TypeOption.number,
-                onChanged: (value) => onChangeAge(ref, value)),
+              type: TypeOption.number,
+              onChanged: (value) => setState(() => onChangeAge(ref, value)),
+            ),
             const SizedBox(height: 15),
             const CustomDivider(color: Colors.black12),
             const SizedBox(height: 15),
             const Text("MyTextFieldWidget"),
             const SizedBox(height: 15),
             MyTextFieldWidget(
-                value: user.name,
-                onChanged: (value) => onChangeName(ref, value)),
+              value: user.name,
+              onChanged: (value) => setState(() => onChangeName(ref, value)),
+            ),
             const SizedBox(height: 15),
             MyTextFieldWidget(
-                type: Type2Option.currency,
-                decimal: 0,
-                value: user.age.toString(),
-                onChanged: (value) => onChangeAge(ref, value)),
+              type: Type2Option.currency,
+              decimal: 0,
+              value: user.age.toString(),
+              onChanged: (value) => setState(() => onChangeAge(ref, value)),
+            ),
             const SizedBox(height: 15),
             const CustomDivider(color: Colors.black12),
             const SizedBox(height: 15),
-            const Text("MyTextFieldWidget"),
+            const Text("MyTextField"),
             const SizedBox(height: 15),
             MyTextField(controller: _nameController),
             const SizedBox(height: 15),
             MyTextField(
-                type: MyTextFieldTypeOption.currency,
-                decimal: 0,
-                controller: _ageController),
+              type: MyTextFieldTypeOption.currency,
+              decimal: 0,
+              controller: _ageController,
+            ),
             const SizedBox(height: 15),
             Center(
               child: Text(user.age.toString()),
